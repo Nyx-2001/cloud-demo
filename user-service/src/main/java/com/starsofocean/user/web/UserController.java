9f84599a39d816d5d89c00168c337bf794f6759f
starsofocean <2865143424@qq.com>
2022-06-07 14:56:33 +0800

    nacos配置管理-配置热更新

--- user-service/src/main/java/com/starsofocean/user/web/UserController.java
+++ user-service/src/main/java/com/starsofocean/user/web/UserController.java
@@ -1,16 +1,19 @@
 package com.starsofocean.user.web;
 
+import com.starsofocean.user.config.PatternProperties;
 import com.starsofocean.user.pojo.User;
 import com.starsofocean.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Slf4j
+//@RefreshScope
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -18,11 +21,14 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @Value("${pattern.dateformat}")
-    private String dateformat;
+//    @Value("${pattern.dateformat}")
+//    private String dateformat;
+    @Autowired
+    private PatternProperties properties;
     @GetMapping("/now")
     public String now(){
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+//        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(properties.getDateformat()));
     }
 
     /**
